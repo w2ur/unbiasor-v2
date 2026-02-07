c795a000c49e3bba6759cdb21b8252fea2712eb2
w2ur <108263423+w2ur@users.noreply.github.com>
2026-02-07 13:34:54 +0100

    Fix Cloudflare Pages adapter configuration for entry-point

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import cloudflarePages from '@astrojs/cloudflare-pages';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
+  adapter: cloudflarePages(),
   integrations: [tailwind(), react()],
   output: 'static',
   site: 'https://unbiasor.com',
